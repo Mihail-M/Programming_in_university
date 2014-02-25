3eea7b64449040fd47ba443484c01f724dafd2db
Mihail-M <Mihail-maryfich@yandex.ru>
2014-02-25 23:15:52 +0300

    Update sorter.h

--- TymofeiBryksin/2 semestr/2 week/sorter/sorter.h
+++ TymofeiBryksin/2 semestr/2 week/sorter/sorter.h
@@ -3,17 +3,15 @@
 class Sorter
 {
 public:
-    Sorter(int *A){
-        array = A;
-    }
+    Sorter(int *A);
 
     virtual int *getSortArray(int l, int r) = 0;
     virtual int *getSortArray(int l, int r, bool (*comp)(int, int)) = 0;
 
 protected:
     int *array;
-    virtual void sort(int *arr, int l, int r) = 0 ;
-    virtual void sort(int *arr, bool (*comp)(int, int) ) = 0;
+    virtual  void sort(int l, int r) = 0;
+    virtual void sort(int l, int r, bool (*comp)(int, int) ) = 0;
 
 
 };
