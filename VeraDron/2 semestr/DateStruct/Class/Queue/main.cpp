e1629ab9dde0c0938c2e563ddbd75d6ea9d65e86
Mihail-M <mihail-maryfich@yandex.ru>
2014-03-04 02:26:58 +0400

    JosephusProblem(143)

--- VeraDron/2 semestr/DateStruct/Class/Queue/main.cpp
+++ VeraDron/2 semestr/DateStruct/Class/Queue/main.cpp
@@ -5,12 +5,20 @@ using namespace std;
 int main()
 {
     Queue a;
-    for(int i = 0; i < 1000; i++)
+    int n, m;
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++)
         a.push(i);
-    for(int i = 0; i < 200; i++)
-        a.pop();
+    while(a.size() > 1) {
 
+        for(int i = 0; i < m-1; i++)
+            a.push(a.pop());
+        cout << a.front() << " ";
+        a.pop();
+    }
     cout << a.front();
+    a.clear();
+
     return 0;
 }
 
