6509a2bbecc4565105cafd27f53ae25831830af6
Mihail-M <mihail-maryfich@yandex.ru>
2014-05-29 23:45:53 +0400

    7.1 -final

--- VeraDron/2 semestr/DateStruct/Class/7/7.1/main.cpp
+++ VeraDron/2 semestr/DateStruct/Class/7/7.1/main.cpp
@@ -7,7 +7,7 @@ public:
      A(double _x1, double _x2):a(max(_x1,_x2)), b(min(_x1,_x2)) {}
      double show(double x) {
         double ans = min(x, b);
-		b = min(max(b, x), max(a, b));
+    	b = min(max(b, x), max(a, b));
 		a = max(a, x);
 		return ans;
     }
@@ -22,13 +22,17 @@ private:
 class B: public A{
 public:
     B(double _x1, double _x2, double _x3): A(_x1, _x2), c(A::show(_x3)) {}
+    
     double show(double x) {
-	if (x < c)
-			
-	    x = c;
-        c = A::show(x);
-        return c;
+	    if (x >= c){
+	        int temp = c;
+            c = A::show(x);
+            return temp;
+        }
+        else
+            return x;
     }
+    
     void print() {
         A::print();
         cout << c << " ";
@@ -42,7 +46,7 @@ int main()
     B b(2, 6, 7);
     b.print();
     cout << endl;
-    cout << b.show(1);
+    cout << b.show(3);
     cout << endl;
     b.print();
     return 0;
