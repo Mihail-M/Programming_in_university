5392ce0f6dadbe58b126763c610e95c9e8059199
Mihail-M <Mihail-maryfich@yandex.ru>
2014-10-28 13:46:16 +0300

    Максимальный поток - без динамической памяти

---
+++ VeraDron/3 semestr/graphs/task_6/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cstdio>
+#include <queue>
+using namespace std;
+
+const int inf = (int)(1e+9);
+
+int n, m, c[1234][1234], f[1234][1234], push[1234], parent[1234];
+
+bool used[1234];
+
+void init(int s) {
+    for(int i = 1; i <= n; i++) {
+        used[i] = parent[i] = push[i] = 0;
+    }
+    push[s] = inf;
+    used[s] = 1;
+    parent[s] = s;
+}
+
+bool bfs(int s, int t) {
+    int k;
+    init(s);
+    queue <int> q;
+    q.push(s);
+
+    while(q.size() > 0 && !used[t]) {
+        k = q.front();
+        q.pop();
+        for(int i = 1; i <= n; i++)
+            if(!used[i] && (c[k][i] - f[k][i] > 0)) {
+                used[i] = 1;
+                push[i] = min(push[k], c[k][i] - f[k][i]);
+                parent[i] = k;
+                q.push(i);
+            }
+    }
+    return used[t];
+}
+
+int max_flow(int s, int t) {
+    int ans = 0;
+
+    while(bfs(s, t)) {
+        int add = push[t];
+        int v = parent[t];
+        int u = t;
+
+        while(v != u) {
+            f[v][u] += add;
+            f[u][v] -= add;
+            u = v;
+            v = parent[u];
+        }
+        ans += add;
+    }
+
+    return ans;
+}
+
+int main() {
+
+    int x, y, w;
+
+    cin >> n >> m;
+
+    for(int i = 0; i < m; i++) {
+        cin >> x >> y >> w;
+        c[x][y] = w;
+    }
+
+    cout << max_flow(1, n);
+    return 0;
+}
