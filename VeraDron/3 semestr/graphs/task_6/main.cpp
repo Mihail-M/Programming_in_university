2f7f71741bdd97d7a6eb092556a423e77005f30a
Mihail-M <Mihail-maryfich@yandex.ru>
2014-12-24 02:24:23 +0300

    исправлено нахождение максимального потока

--- VeraDron/3 semestr/graphs/task_6/main.cpp
+++ VeraDron/3 semestr/graphs/task_6/main.cpp
@@ -1,31 +1,75 @@
 #include <iostream>
 #include <cstdio>
 #include <queue>
-#include "flow.h"
-
 using namespace std;
-using namespace Flow;
 
+const int inf = (int)(1e+9);
+
+int n, m, c[1050][1050], f[1050][1050]
+        , push[1050], parent[1050];
+
+bool used[1050];
+
+void init(int s) {
+    for(int i = 1; i <= n; i++) {
+        used[i] = parent[i] = push[i] = 0;
+    }
+    push[s] = inf;
+    used[s] = 1;
+    parent[s] = s;
+}
+
+bool bfs(int s, int t) {
+    int k;
+    init(s);
+    queue <int> q;
+    q.push(s);
+
+    while(q.size() > 0 && !used[t]) {
+        k = q.front();
+        q.pop();
+        for(int i = 1; i <= n; i++)
+            if(!used[i] && (c[k][i] - f[k][i] > 0)) {
+                used[i] = 1;
+                push[i] = min(push[k], c[k][i] - f[k][i]);
+                parent[i] = k;
+                q.push(i);
+            }
+    }
+    return used[t];
+}
+
+int max_flow(int s, int t) {
+    int ans = 0;
+
+    while(bfs(s, t)) {
+        int add = push[t];
+        int v = parent[t];
+        int u = t;
+
+        while(v != u) {
+            f[v][u] += add;
+            f[u][v] -= add;
+            u = v;
+            v = parent[u];
+        }
+        ans += add;
+    }
+
+    return ans;
+}
 
 int main() {
 
-    int n, m;
+    int x, y, w;
+
     cin >> n >> m;
-    int **c;
-    c = new int*[n + 1];
-    for (int i = 0; i < n; i++)
-        c[i] = new int[n];
 
     for(int i = 0; i < m; i++) {
-        int x, y, w;
         cin >> x >> y >> w;
-        c[x - 1][y - 1] = w;
+        c[x][y] = w;
     }
 
-    cout << max_flow(0, n - 1, n, c);
-
-    for(int i = 0; i < n; i++)
-        delete[] c[i];
-    delete[] c;
+    cout << max_flow(1, n);
     return 0;
 }
